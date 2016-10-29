d0577fdd399ed9d916ae9f76c3b3ec487a5984cc
MunifTanjim <MunifTanjim@users.noreply.github.com>
2016-10-29 20:53:21 +0600

    tweak and cleanup configs

--- bash-it.bash
+++ bash-it.bash
@@ -1,36 +1,24 @@
-# Path to the bash it configuration
 export BASH_IT="$HOME/.dotfiles/.bash-it"
-
-# Lock and Load a custom theme file
-# location /.bash_it/themes/
 export BASH_IT_THEME='brainy'
 
-# (Advanced): Change this to the name of your remote repo if you
-# cloned bash-it with a remote other than origin such as `bash-it`.
-# export BASH_IT_REMOTE='bash-it'
+export THEME_SHOW_CLOCK="false"
 
-# Your place for hosting Git repos. I use this for private repos.
+# export BASH_IT_REMOTE='origin'
+
 # export GIT_HOSTING='git@git.domain.com'
 
-# Don't check mail when opening terminal.
 unset MAILCHECK
 
-# Change this to your console based IRC client of choice.
-# %export IRC_CLIENT='irssi'
+# export IRC_CLIENT='irssi'
 
-# Set this to the command you use for todo.txt-cli
 export TODO="t"
 
-# Set this to false to turn off version control status checking within the prompt for all themes
 export SCM_CHECK=true
 
 # Set vcprompt executable path for scm advance info in prompt (demula theme)
 # https://github.com/djl/vcprompt
 #export VCPROMPT_EXECUTABLE=~/.vcprompt/bin/vcprompt
 
-# (Advanced): Uncomment this to make Bash-it reload itself automatically
-# after enabling or disabling aliases, plugins, and completions.
 export BASH_IT_AUTOMATIC_RELOAD_AFTER_CONFIG_CHANGE=1
 
-# Load Bash It
 source $BASH_IT/bash_it.sh
