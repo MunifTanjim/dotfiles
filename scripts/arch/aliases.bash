33974f5882337580a9153b2714ba92c6e2debd8c
MunifTanjim <MunifTanjim@users.noreply.github.com>
2017-06-17 20:52:15 +0600

    remove unnecessary files

--- scripts/arch/aliases.bash
+++
@@ -1,4 +0,0 @@
-# pacman aliases
-alias pacman-remove-orphans='sudo pacman -Rns $(pacman -Qtdq)'
-alias pacman-unlock='sudo rm /var/lib/pacman/db.lck'
-
--- scripts/arch/bashrc.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export DOTFILES="${HOME}/.dotfiles"
-
-source "${DOTFILES}/scripts/arch/arch.bash"
-source "${DOTFILES}/scripts/paths.bash"
-source "${DOTFILES}/modules/init.sh"
