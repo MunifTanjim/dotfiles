69874b1ee316c86610d667a2b005b47a5476d06c
MunifTanjim <muniftanjim@gmail.com>
2019-08-27 01:16:10 +0600

    replace liquidprompt with own fork

--- modules/themes/liquidprompt.bash
+++ modules/themes/liquidprompt.bash
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-#source "${DOTFILES_MODULES}/.liquidprompt/liquidprompt"
+source "${DOTFILES_MODULES}/.liquidprompt/liquidprompt"
 #source "${DOTFILES_MODULES}/themes/liquidprompt.mod.bash"
-source "${HOME}/data/dev/gits/liquidprompt/liquidprompt"
