fe20e09b83ef7490799e13c7464bd761c4e9a34a
Munif Tanjim <muniftanjim@gmail.com>
2020-04-25 20:16:47 +0600

    add config: rofunicode

---
+++ private_dot_config/rofunicode/config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+export ROFUNICODE_DATA_FILENAMES="emojis"
+export ROFUNICODE_PROMPT="Emoji"
+export ROFUNICODE_SKIN_TONE="medium-light" # neutral/light/medium-light/medium/medium-dark/dark
