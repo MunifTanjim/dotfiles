a3ab347036b164cd0e16fc455eec7fcf4a61cd59
Munif Tanjim <muniftanjim@gmail.com>
2020-08-22 23:23:03 +0600

    update module: rust

--- .modules/rust.plugin.sh
+++ .modules/rust.plugin.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=sh
 
-export CARGO_HOME=${CARGO_HOME:-"${HOME}/.cargo"}
-export RUSTUP_HOME=${RUSTUP_HOME:-"${HOME}/.rustup"}
+export CARGO_HOME=${CARGO_HOME:-"${HOME}/.local/share/cargo"}
+export RUSTUP_HOME=${RUSTUP_HOME:-"${HOME}/.local/share/rustup"}
 
 if directory_exists "${CARGO_HOME}"; then
   . "${CARGO_HOME}/env"
