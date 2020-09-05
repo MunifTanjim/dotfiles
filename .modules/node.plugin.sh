f1f3d0c46d82691e22ddf4aa4a6ca91138aa3812
Munif Tanjim <muniftanjim@gmail.com>
2020-09-05 20:47:13 +0600

    update module: node

--- .modules/node.plugin.sh
+++ .modules/node.plugin.sh
@@ -5,17 +5,17 @@
 
 export NVM_DIR="${HOME}/.local/share/nvm"
 
-if directory_exists "${NVM_DIR}"; then
-  __init_nvm() {
+if [ -d "${NVM_DIR}" ]; then
+  __load_nvm() {
     if [ -s "${NVM_DIR}/nvm.sh" ]; then
-      . "$NVM_DIR/nvm.sh"
+      . "${NVM_DIR}/nvm.sh"
     fi
   }
 
-  __node_global_bins=("nvm" "node" $(find ${NVM_DIR}/versions/node -maxdepth 3 -path '*/bin/*' -type l 2>/dev/null | xargs -n1 basename | sort | uniq))
+  __node_global_bins=("nvm" "node" $(find ${NVM_DIR}/versions/node -maxdepth 3 -path '*/bin/*' -type l -print0 2>/dev/null | xargs -0 -n1 basename | sort | uniq))
 
-  for bin in "${NODE_GLOBALS[@]}"; do
-    eval "${bin}(){ unset -f ${__node_global_bins} >/dev/null 2>&1; __init_nvm; ${bin} \$@; }"
+  for bin in "${__node_global_bins[@]}"; do
+    eval "${bin}(){ unset -f ${__node_global_bins[@]} >/dev/null 2>&1; __load_nvm; unset __load_nvm __node_global_bins; ${bin} \$@; }"
   done
 fi
 
