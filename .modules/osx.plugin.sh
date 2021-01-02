0699e05c722bc63c5eb9ad57d015a532240390aa
Munif Tanjim <muniftanjim@gmail.com>
2021-01-02 18:24:34 +0600

    add module: osx

---
+++ .modules/osx.plugin.sh
@@ -0,0 +1,14 @@
+# shellcheck shell=sh
+
+# openssl
+OSX_OPENSSL_VERSION="${OSX_OPENSSL_VERSION:-"1.1"}"
+brew_openssl_prefix="$(brew --prefix openssl@${OSX_OPENSSL_VERSION})"
+pathmunge "${brew_openssl_prefix}/bin"
+export CFLAGS="-I${brew_openssl_prefix}/include"
+export CPPFLAGS="-I${brew_openssl_prefix}/include"
+export LDFLAGS="-L${brew_openssl_prefix}/lib"
+export PKG_CONFIG_PATH="${brew_openssl_prefix}/lib/pkgconfig"
+unset brew_openssl_prefix
+
+# coreutils
+pathmunge "/usr/local/opt/coreutils/libexec/gnubin"
