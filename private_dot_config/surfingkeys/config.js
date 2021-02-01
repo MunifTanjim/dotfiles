a25ffe44b416b139744a2deb667734a5c7fcd727
Munif Tanjim <muniftanjim@gmail.com>
2021-02-02 03:13:10 +0600

    add bin: open

---
+++ private_dot_config/surfingkeys/config.js
@@ -0,0 +1,43 @@
+settings.hintAlign = 'left'
+settings.modeAfterYank = 'Normal'
+settings.omnibarPosition = 'bottom'
+
+settings.theme = `
+.sk_theme {
+    font-family: Input Sans Condensed, Charcoal, sans-serif;
+    font-size: 10pt;
+    background: #24272e;
+    color: #abb2bf;
+}
+.sk_theme tbody {
+    color: #fff;
+}
+.sk_theme input {
+    color: #d0d0d0;
+}
+.sk_theme .url {
+    color: #61afef;
+}
+.sk_theme .annotation {
+    color: #56b6c2;
+}
+.sk_theme .omnibar_highlight {
+    color: #528bff;
+}
+.sk_theme .omnibar_timestamp {
+    color: #e5c07b;
+}
+.sk_theme .omnibar_visitcount {
+    color: #98c379;
+}
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+    background: #303030;
+}
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+    background: #3e4452;
+}
+#sk_status, #sk_find {
+    font-size: 20pt;
+}
+`
+iunmap(':')
