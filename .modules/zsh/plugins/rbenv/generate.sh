8b3b9b315b89fd8dd8579c8b6534b4049f031d76
Munif Tanjim <hello@muniftanjim.dev>
2021-11-16 11:14:20 +0600

    update module: zsh/plugins/rbenv

--- .modules/zsh/plugins/rbenv/generate.sh
+++ .modules/zsh/plugins/rbenv/generate.sh
@@ -1,6 +1,8 @@
 # shellcheck shell=sh
 
-declare filename="$(dirname $0)/_generated.zsh"
+__generated_file="$(dirname $(realpath $0))/_generated.zsh"
 
-rm -f "${filename}"
-rbenv init - zsh >> "${filename}"
+rm -f "${__generated_file}"
+rbenv init - zsh >> "${__generated_file}"
+
+unset __generated_file
