7a55aefa29188ec249c68d7d438b0dd831d9c1d0
Munif Tanjim <hello@muniftanjim.dev>
2022-03-02 11:34:52 +0600

    (nvim): update nvim-tree config

--- private_dot_config/nvim/lua/config/tree.lua
+++ private_dot_config/nvim/lua/config/tree.lua
@@ -1,11 +1,9 @@
-vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_root_folder_modifier = ":~"
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_group_empty = 0
-vim.g.nvim_tree_disable_window_picker = 1
 vim.g.nvim_tree_icon_padding = " "
 vim.g.nvim_tree_symlink_arrow = " ➛ "
 vim.g.nvim_tree_respect_buf_cwd = 0
@@ -90,6 +88,14 @@ require("nvim-tree").setup({
     mappings = {
       custom_only = true,
       list = mapping_list,
+    },
+  },
+  actions = {
+    open_file = {
+      quit_on_open = true,
+      window_picker = {
+        enable = false,
+      },
     },
   },
 })
