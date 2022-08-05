5287c3ca89b7f773a3d8af6d8d78f3fe1c47062c
Munif Tanjim <hello@muniftanjim.dev>
2022-08-05 23:57:38 +0600

    (go): fix plugin for darwin

--- .modules/go.plugin.sh
+++ .modules/go.plugin.sh
@@ -1,8 +1,13 @@
 # shellcheck shell=sh
 
-GOLANG_DIR="${GOLANG_DIR:-"${XDG_DATA_HOME:-"${HOME}/.local/share"}/golang"}"
+if command_exists go; then
+  export GOROOT="${GOROOT:-"$(go env GOROOT)"}"
+else
+  GOLANG_DIR="${GOLANG_DIR:-"${XDG_DATA_HOME:-"${HOME}/.local/share"}/golang"}"
+  export GOROOT="${GOROOT:-"${GOLANG_DIR}/current"}"
+fi
 
-export GOROOT=${GOROOT:-"${GOLANG_DIR}/current"}
-pathmunge "${GOROOT}/bin"
 export GOPATH="${GOPATH:-"${XDG_DATA_HOME:-"${HOME}/.local/share"}/go"}"
+
+pathmunge "${GOROOT}/bin"
 pathmunge "${GOPATH}/bin"
