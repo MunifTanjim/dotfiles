b1d1bb14c086ff4c08bd569a4ef8a474c223ce26
Munif Tanjim <hello@muniftanjim.dev>
2022-08-16 02:17:40 +0600

    (nvim): update telescope config

--- private_dot_config/nvim/lua/config/plugins/telescope.lua
+++ private_dot_config/nvim/lua/config/plugins/telescope.lua
@@ -14,12 +14,13 @@ telescope.setup({
   extensions = {
     fzf = {
       fuzzy = true,
-      override_generic_sorter = false,
+      override_generic_sorter = true,
       override_file_sorter = true,
       case_mode = "smart_case",
     },
     frecency = {
       default_workspace = "LSP",
+      show_unindexed = false,
     },
   },
   pickers = {
