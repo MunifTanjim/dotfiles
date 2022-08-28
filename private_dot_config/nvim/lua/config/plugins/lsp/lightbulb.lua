10b7bd6629d2f4f82537ac5cde8a1c71269d1c06
Munif Tanjim <hello@muniftanjim.dev>
2022-08-28 23:10:44 +0600

    (nvim): disable lightbulb for c,cpp

--- private_dot_config/nvim/lua/config/plugins/lsp/lightbulb.lua
+++ private_dot_config/nvim/lua/config/plugins/lsp/lightbulb.lua
@@ -3,14 +3,29 @@ vim.fn.sign_define("LightBulbSign", {
   texthl = "DiagnosticSignHint",
 })
 
-require("nvim-lightbulb").setup({
+local lightbulb = require("nvim-lightbulb")
+
+lightbulb.setup({
   sign = {
     enabled = true,
     priority = 10,
   },
-  autocmd = {
-    enabled = true,
-    pattern = { "*" },
-    events = { "CursorHold", "CursorHoldI" },
-  },
+})
+
+-- disabled because of unusual offset_encoding of `clangd` server causing warning in:
+-- https://github.com/neovim/neovim/blob/88c32b5/runtime/lua/vim/lsp/util.lua#L1882-L1886
+local is_disabled_filetype = {
+  c = true,
+  cpp = true,
+}
+
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  pattern = { "*" },
+  group = vim.api.nvim_create_augroup("LightBulb", { clear = true }),
+  desc = "lua require('nvim-lightbulb').update_lightbulb()",
+  callback = function(params)
+    if not is_disabled_filetype[vim.bo[params.buf].filetype] then
+      lightbulb.update_lightbulb()
+    end
+  end,
 })
