160f61eb22cd4f06cb87b2e749be95460102dfa0
Munif Tanjim <hello@muniftanjim.dev>
2022-08-28 23:24:12 +0600

    (nvim): use explicit offset_encoding for hover

--- private_dot_config/nvim/lua/config/lsp/custom.lua
+++ private_dot_config/nvim/lua/config/lsp/custom.lua
@@ -100,4 +100,10 @@ function mod.document_highlight(offset_encoding)
   vim.lsp.buf_request(0, "textDocument/documentHighlight", params, nil)
 end
 
+---@param offset_encoding 'utf-8'|'utf-16'|'utf-32'
+function mod.hover(offset_encoding)
+  local params = vim.lsp.util.make_position_params(0, offset_encoding)
+  vim.lsp.buf_request(0, "textDocument/hover", params, nil)
+end
+
 return mod
