54d348d8ebf7b9dccd1ff61c628b1e67447185cc
Munif Tanjim <hello@muniftanjim.dev>
2022-10-17 11:24:03 +0600

    (nvim): fix treesitter highlight groups

--- private_dot_config/nvim/lua/config/init.lua
+++ private_dot_config/nvim/lua/config/init.lua
@@ -7,66 +7,68 @@ local function appearance_settings()
   -- treesitter
   vim.schedule(function()
     vim.cmd([[
-      " hi! link TSAnnotation
-      " hi! link TSAttribute
-      hi! link TSBoolean Boolean
-      hi! link TSCharacter Character
-      hi! link TSComment Comment
-      " hi! link TSConstructor
-      hi! link TSConditional Conditional
-      hi! link TSConstant Constant
-      hi! link TSConstBuiltin Constant
-      hi! link TSConstMacro Constant
-      " hi! link TSConstructor
-      hi! link TSError GruvboxRedUnderline
-      hi! link TSException Exception
-      " hi! link TSField
-      hi! link TSFloat Float
-      hi! link TSFunction Function
-      hi! link TSFuncBuiltin Function
-      " hi! link TSFuncMacro
-      hi! link TSInclude Include
-      hi! link TSKeyword GruvboxRed
-      hi! link TSKeywordFunction GruvboxAqua
-      hi! link TSKeywordOperator TSOperator
-      hi! link TSLabel Label
-      " hi! link TSMethod
-      " hi! link TSNamespace
-      " hi! link TSNone
-      hi! link TSNumber Number
-      " hi! link TSOperator
-      " hi! link TSParameter
-      " hi! link TSParameterReference
-      hi! link TSProperty GruvboxBlue
-      hi! link TSPunctBracket GruvboxFg1
-      hi! link TSPunctDelimiter GruvboxFg1
-      " hi! link TSPunctSpecial
-      hi! link TSRepeat Repeat
-      hi! link TSString String
-      " hi! link TSStringRegex
-      " hi! link TSStringEscape
-      " hi! link TSSymbol
-      hi! link TSTag GruvboxGreen
-      " hi! link TSTagDelimiter
-      " hi! link TSText
-      hi! TSStrong term=bold cterm=bold gui=bold
-      hi! TSEmphasis term=italic cterm=italic gui=italic
-      hi! TSUnderline term=underline cterm=underline gui=underline
-      hi! TSStrike term=strikethrough cterm=strikethrough gui=strikethrough
-      " hi! link TSTitle
-      " hi! link TSLiteral
-      " hi! link TSURI
-      " hi! link TSMath
-      " hi! link TSTextReference
-      " hi! link TSEnviroment
-      " hi! link TSEnviromentName
-      " hi! link TSNote
-      hi! link TSWarning WarningMsg
-      hi! link TSDanger ErrorMsg
-      hi! link TSType GruvboxAqua
-      hi! link TSTypeBuiltin Type
-      hi! link TSVariable GruvboxFg1
-      hi! link TSVariableBuiltin Identifier
+      hi! link @annotation PreProc
+      hi! link @attribute PreProc
+      hi! link @boolean Boolean
+      hi! link @character Character
+      hi! link @comment Comment
+      hi! link @conditional Conditional
+      hi! link @constant Constant
+      hi! link @constant.builtin Constant
+      hi! link @constant.macro Constant
+      hi! link @constructor Special
+      hi! link @error GruvboxRedUnderline
+      hi! link @exception Exception
+      hi! link @field Identifier
+      hi! link @float Float
+      hi! link @function Function
+      hi! link @function.builtin Function
+      hi! link @function.macro Macro
+      hi! link @include Include
+      hi! link @keyword GruvboxRed
+      hi! link @keyword.function GruvboxAqua
+      hi! link @keyword.operator @operator
+      hi! link @label Label
+      hi! link @method Function
+      hi! link @method.call @method
+      hi! link @namespace Include
+      " hi! link @none
+      hi! link @number Number
+      hi! link @operator Operator
+      hi! link @parameter Identifier
+      hi! link @parameter.reference @parameter
+      hi! link @property GruvboxBlue
+      hi! link @punctuation.bracket GruvboxFg3
+      hi! link @punctuation.delimiter GruvboxFg3
+      hi! link @punctuation.special GruvboxOrange
+      hi! link @repeat Repeat
+      hi! link @string String
+      hi! link @string.regex String
+      hi! link @string.escape SpecialChar
+      hi! link @string.special SpecialChar
+      hi! link @symbol Identifier
+      hi! link @tag GruvboxGreen
+      hi! link @tag.attribute @property
+      hi! link @tag.delimiter @punctuation.delimiter
+      " hi! link @text
+      hi! @text.strong term=bold cterm=bold gui=bold
+      hi! @text.emphasis term=italic cterm=italic gui=italic
+      hi! @text.underline term=underline cterm=underline gui=underline
+      hi! @text.strike term=strikethrough cterm=strikethrough gui=strikethrough
+      hi! link @text.title Title
+      hi! link @text.literal String
+      hi! link @text.uri Underlined
+      hi! link @text.math Special
+      hi! link @text.reference Constant
+      hi! link @text.environment Macro
+      hi! link @text.environment.name Type
+      hi! link @text.note SpecialComment
+      hi! link @text.warning WarningMsg
+      hi! link @text.danger ErrorMsg
+      hi! link @type GruvboxAqua
+      hi! link @type.builtin Type
+      hi! link @variable GruvboxFg1
+      hi! link @variable.builtin Identifier
     ]])
   end)
 end
