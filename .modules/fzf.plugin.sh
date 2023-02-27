c0033499ca83befdb340af82cc7cf9737b8e795f
Munif Tanjim <hello@muniftanjim.dev>
2023-02-27 22:42:26 +0600

    (fzf): update keybinds for preview scroll

--- .modules/fzf.plugin.sh
+++ .modules/fzf.plugin.sh
@@ -9,7 +9,11 @@ fi
 export FZF_DEFAULT_OPTS='--color=dark
 --color=bg:#1d2021,bg+:#3c3836,fg:#fbf1c7,fg+:#fbf1c7,hl:#d79921,hl+:#d65d0e
 --color=gutter:#3c3836,info:#b16286,border:#b16286,prompt:#458588,pointer:#d3869b
---color=marker:#b16286,spinner:#689d6a,header:#458588'
+--color=marker:#b16286,spinner:#689d6a,header:#458588
+--bind ctrl-y:preview-up,ctrl-e:preview-down
+--bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down
+--bind ctrl-b:preview-page-up,ctrl-f:preview-page-down
+'
 
 if command_exists fd; then
   export FZF_DEFAULT_COMMAND='fd --type f --hidden'
