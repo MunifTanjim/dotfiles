3b64c7489539d7443ff89fd2af651d177e136386
Munif Tanjim <hello@muniftanjim.dev>
2023-11-09 02:51:19 +0600

    (nvim): update telescope layout config

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -67,74 +67,226 @@ function plugin.init()
 end
 
 function plugin.config()
+  local Layout = require("nui.layout")
+  local Popup = require("nui.popup")
+
   local telescope = require("telescope")
+  local TSLayout = require("telescope.pickers.layout")
 
-  local layout_strategies = require("telescope.pickers.layout_strategies")
-
-  layout_strategies._vertical = layout_strategies.vertical
-  function layout_strategies.vertical(...)
-    local ret = layout_strategies._vertical(...)
-
-    if ret.preview then
-      ret.preview.border = { 1, 1, 0, 1 }
-      ret.preview.borderchars = vim.deepcopy(ret.preview.borderchars)
-      ret.preview.height = ret.preview.height + 1
+  local function make_popup(options)
+    local popup = Popup(options)
+    function popup.border:change_title(title)
+      popup.border.set_text(popup.border, "top", title)
     end
-
-    ret.results.border = { 1, 1, 0, 1 }
-    ret.results.borderchars = vim.deepcopy(ret.results.borderchars)
-    ret.results.borderchars[5] = "├"
-    ret.results.borderchars[6] = "┤"
-    ret.results.height = ret.results.height + 1
-
-    ret.prompt.borderchars = vim.deepcopy(ret.prompt.borderchars)
-    ret.prompt.borderchars[5] = "├"
-    ret.prompt.borderchars[6] = "┤"
-
-    return ret
-  end
-
-  layout_strategies._horizontal = layout_strategies.horizontal
-  function layout_strategies.horizontal(...)
-    local ret = layout_strategies._horizontal(...)
-
-    if ret.preview then
-      ret.preview.border = { 1, 1, 1, 0 }
-      ret.preview.borderchars = vim.deepcopy(ret.preview.borderchars)
-      ret.preview.col = ret.preview.col - 1
-      ret.preview.width = ret.preview.width + 1
-    end
-
-    ret.results.border = { 1, 1, 0, 1 }
-    ret.results.borderchars = vim.deepcopy(ret.results.borderchars)
-    ret.results.borderchars[6] = "┬"
-    ret.results.height = ret.results.height + 1
-
-    ret.prompt.borderchars = vim.deepcopy(ret.prompt.borderchars)
-    ret.prompt.borderchars[5] = "├"
-    ret.prompt.borderchars[6] = "┤"
-    ret.prompt.borderchars[7] = "┴"
-
-    return ret
+    return TSLayout.Window(popup)
   end
 
   telescope.setup({
     defaults = {
+      layout_strategy = "flex",
       layout_config = {
         horizontal = {
-          width = 0.9,
-          height = 0.6,
-          preview_cutoff = 120,
+          size = {
+            width = "90%",
+            height = "60%",
+          },
         },
         vertical = {
-          width = 0.9,
-          height = 0.9,
-        },
-        flex = {
-          flip_columns = 120,
+          size = {
+            width = "90%",
+            height = "90%",
+          },
         },
       },
-      layout_strategy = "flex",
+      create_layout = function(picker)
+        local border = {
+          results = {
+            top_left = "┌",
+            top = "─",
+            top_right = "┬",
+            right = "│",
+            bottom_right = "",
+            bottom = "",
+            bottom_left = "",
+            left = "│",
+          },
+          results_patch = {
+            minimal = {
+              top_left = "┌",
+              top_right = "┐",
+            },
+            horizontal = {
+              top_left = "┌",
+              top_right = "┬",
+            },
+            vertical = {
+              top_left = "├",
+              top_right = "┤",
+            },
+          },
+          prompt = {
+            top_left = "├",
+            top = "─",
+            top_right = "┤",
+            right = "│",
+            bottom_right = "┘",
+            bottom = "─",
+            bottom_left = "└",
+            left = "│",
+          },
+          prompt_patch = {
+            minimal = {
+              bottom_right = "┘",
+            },
+            horizontal = {
+              bottom_right = "┴",
+            },
+            vertical = {
+              bottom_right = "┘",
+            },
+          },
+          preview = {
+            top_left = "┌",
+            top = "─",
+            top_right = "┐",
+            right = "│",
+            bottom_right = "┘",
+            bottom = "─",
+            bottom_left = "└",
+            left = "│",
+          },
+          preview_patch = {
+            minimal = {},
+            horizontal = {
+              bottom = "─",
+              bottom_left = "",
+              bottom_right = "┘",
+              left = "",
+              top_left = "",
+            },
+            vertical = {
+              bottom = "",
+              bottom_left = "",
+              bottom_right = "",
+              left = "│",
+              top_left = "┌",
+            },
+          },
+        }
+
+        local results = make_popup({
+          focusable = false,
+          border = {
+            style = border.results,
+            text = {
+              top = picker.results_title,
+              top_align = "center",
+            },
+          },
+          win_options = {
+            winhighlight = "Normal:Normal",
+          },
+        })
+
+        local prompt = make_popup({
+          enter = true,
+          border = {
+            style = border.prompt,
+            text = {
+              top = picker.prompt_title,
+              top_align = "center",
+            },
+          },
+          win_options = {
+            winhighlight = "Normal:Normal",
+          },
+        })
+
+        local preview = make_popup({
+          focusable = false,
+          border = {
+            style = border.preview,
+            text = {
+              top = picker.preview_title,
+              top_align = "center",
+            },
+          },
+        })
+
+        local box_by_kind = {
+          vertical = Layout.Box({
+            Layout.Box(preview, { grow = 1 }),
+            Layout.Box(results, { grow = 1 }),
+            Layout.Box(prompt, { size = 3 }),
+          }, { dir = "col" }),
+          horizontal = Layout.Box({
+            Layout.Box({
+              Layout.Box(results, { grow = 1 }),
+              Layout.Box(prompt, { size = 3 }),
+            }, { dir = "col", size = "50%" }),
+            Layout.Box(preview, { size = "50%" }),
+          }, { dir = "row" }),
+          minimal = Layout.Box({
+            Layout.Box(results, { grow = 1 }),
+            Layout.Box(prompt, { size = 3 }),
+          }, { dir = "col" }),
+        }
+
+        local function get_box()
+          local strategy = picker.layout_strategy
+          if strategy == "vertical" or strategy == "horizontal" then
+            return box_by_kind[strategy], strategy
+          end
+
+          local height, width = vim.o.lines, vim.o.columns
+          local box_kind = "horizontal"
+          if width < 100 then
+            box_kind = "vertical"
+            if height < 40 then
+              box_kind = "minimal"
+            end
+          end
+          return box_by_kind[box_kind], box_kind
+        end
+
+        local function prepare_layout_parts(layout, box_type)
+          layout.results = results
+          results.border:set_style(border.results_patch[box_type])
+
+          layout.prompt = prompt
+          prompt.border:set_style(border.prompt_patch[box_type])
+
+          if box_type == "minimal" then
+            layout.preview = nil
+          else
+            layout.preview = preview
+            preview.border:set_style(border.preview_patch[box_type])
+          end
+        end
+
+        local function get_layout_size(box_kind)
+          return picker.layout_config[box_kind == "minimal" and "vertical" or box_kind].size
+        end
+
+        local box, box_kind = get_box()
+        local layout = Layout({
+          relative = "editor",
+          position = "50%",
+          size = get_layout_size(box_kind),
+        }, box)
+
+        layout.picker = picker
+        prepare_layout_parts(layout, box_kind)
+
+        local layout_update = layout.update
+        function layout:update()
+          local box, box_kind = get_box()
+          prepare_layout_parts(layout, box_kind)
+          layout_update(self, { size = get_layout_size(box_kind) }, box)
+        end
+
+        return TSLayout(layout)
+      end,
       mappings = {
         i = {
           ["<C-s>"] = "select_horizontal",
