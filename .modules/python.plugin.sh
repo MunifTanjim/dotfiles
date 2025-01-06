ba5356b9a90967cad890fd75acbcd667cb7b0bc2
Munif Tanjim <hello@muniftanjim.dev>
2025-01-06 15:21:10 +0600

    (python): update plugins for poetry

--- .modules/python.plugin.sh
+++ .modules/python.plugin.sh
@@ -1,5 +1,9 @@
 # shellcheck shell=sh
 
+export POETRY_CACHE_DIR="${XDG_CACHE_HOME:-"${HOME}/.cache"}/pypoetry"
+export POETRY_CONFIG_DIR="${XDG_CONFIG_HOME:-"${HOME}/.config"}/pypoetry"
+export POETRY_DATA_DIR="${XDG_DATA_HOME:-"${HOME}/.local/share"}/pypoetry"
+
 export PYENV_ROOT=${PYENV_ROOT:-"${HOME}/.pyenv"}
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 export VIRTUAL_ENV_DISABLE_PROMPT=1
--- .modules/zsh/plugins/python/python.plugin.zsh
+++ .modules/zsh/plugins/python/python.plugin.zsh
@@ -1,9 +1,13 @@
+export POETRY_CACHE_DIR="${XDG_CACHE_HOME:-"${HOME}/.cache"}/pypoetry"
+export POETRY_CONFIG_DIR="${XDG_CONFIG_HOME:-"${HOME}/.config"}/pypoetry"
+export POETRY_DATA_DIR="${XDG_DATA_HOME:-"${HOME}/.local/share"}/pypoetry"
+
 export PYENV_ROOT=${PYENV_ROOT:-"${HOME}/.pyenv"}
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 if [[ -d "${PYENV_ROOT}/bin" ]]; then
-  PATH="${PYENV_ROOT}/bin:${PATH}"
+  export PATH="${PYENV_ROOT}/bin:${PATH}"
 fi
 
 if (( ${+commands[pyenv]} )); then
