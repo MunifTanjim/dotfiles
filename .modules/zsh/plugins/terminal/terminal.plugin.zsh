7eac846a877cd1e593b8732d516630514f50abbc
Munif Tanjim <hello@muniftanjim.dev>
2025-01-06 15:18:43 +0600

    (ghostty): introduce and integrate

--- .modules/zsh/plugins/terminal/terminal.plugin.zsh
+++ .modules/zsh/plugins/terminal/terminal.plugin.zsh
@@ -1,6 +1,9 @@
-if test -n "${KITTY_INSTALLATION_DIR}"; then
+if test -n "${GHOSTTY_RESOURCES_DIR}"; then
+  builtin source "${GHOSTTY_RESOURCES_DIR}/shell-integration/zsh/ghostty-integration"
+elif test -n "${KITTY_INSTALLATION_DIR}"; then
   export KITTY_SHELL_INTEGRATION="enabled"
   autoload -Uz -- "${KITTY_INSTALLATION_DIR}/shell-integration/zsh/kitty-integration"
   kitty-integration
   unfunction kitty-integration
 fi
+
