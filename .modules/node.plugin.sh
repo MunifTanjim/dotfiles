ab5fc3cba9f5d40503a1b3d9a0d8d9144e8c92ee
Munif Tanjim <hello@muniftanjim.dev>
2025-05-21 12:39:42 +0600

    (node): update fnm integration

--- .modules/node.plugin.sh
+++ .modules/node.plugin.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=sh
 
 export FNM_DIR="${FNM_DIR:-"${XDG_DATA_HOME:-"${HOME}/.local/share"}/fnm"}"
-eval "$(fnm env)"
+eval "$(fnm env --use-on-cd --resolve-engines --corepack-enabled --log-level error)"
 
 export COREPACK_ENABLE_AUTO_PIN=0
 
