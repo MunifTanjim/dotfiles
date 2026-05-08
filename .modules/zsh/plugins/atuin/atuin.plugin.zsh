9ad46dc09ede65163dd35be8bf88fa47fda0b8e6
Munif Tanjim <hello@muniftanjim.dev>
2026-05-08 11:38:47 +0600

    (atuin) update config

--- .modules/zsh/plugins/atuin/atuin.plugin.zsh
+++ .modules/zsh/plugins/atuin/atuin.plugin.zsh
@@ -1,10 +1,5 @@
 source "${0:A:h}/_generated.zsh"
 
-_zsh_autosuggest_strategy_atuin() {
-  suggestion="$(atuin search --cmd-only --limit 1 --search-mode prefix --filter-mode workspace -- "${1}" || \
-                atuin search --cmd-only --limit 1 --search-mode prefix -- "${1}")"
-}
-
 bindkey -M emacs '^r' atuin-search
 bindkey -M viins '^r' atuin-search-viins
 bindkey -M vicmd '/' atuin-search
--- .modules/zsh/plugins/atuin/generate.sh
+++ .modules/zsh/plugins/atuin/generate.sh
@@ -3,6 +3,6 @@
 __generated_file="$(dirname $(realpath $0))/_generated.zsh"
 
 rm -f "${__generated_file}"
-ATUIN_NOBIND=true atuin init zsh >>"${__generated_file}"
+ATUIN_NOBIND=true atuin init zsh --disable-ai >>"${__generated_file}"
 
 unset __generated_file
